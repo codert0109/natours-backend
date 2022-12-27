6c8d79113e997d68bf0e4bb6bcffbca3f51dd126
Arun Bohra <arunbohra12@gmail.com>
2022-12-27 14:32:38 +0530

    Update client production url

--- config/constants.js
+++ config/constants.js
@@ -1,6 +1,6 @@
 const clientUrl =
   process.env.NODE_ENV === 'production'
-    ? 'https://natours-arun.netlify.app'
+    ? 'https://arunbohra-natours.netlify.app'
     : 'http://127.0.0.1:5173';
 
 exports.CLIENT_BASE_URL = clientUrl;
